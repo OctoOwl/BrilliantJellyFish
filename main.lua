3685f03a760df0a202fc83cbd50033cb3ff296b6
Lorelei McKinney <loreleiseda@gmail.com>
2016-03-01 20:58:49 -0800

    Added spinning Jelly, press arrow keys to move :D

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+rotation = 0
+
 function love.conf(t)
     t.title = "BrilliantJellyFish"
     t.version = "0.1"
@@ -6,12 +8,25 @@ function love.conf(t)
 end
 
 function love.load()
-    creeper = love.graphics.newImage("resources/EvilCreeper.png")
+    brillJelly = love.graphics.newImage("resources/BJFSprite.png")
 end
 
---Functions- blocks of computation can have inputs and outputs, Strings- human readables, numbers- numbers, comments- make them by -- are notes
---Drawables- drawn with the draw function (refer to wiki)
+function love.update(dt)
+    if love.keyboard.isDown("escape") then
+        love.event.push('quit')
+    end
+
+    if love.keyboard.isDown("left", "a") then
+        rotation = rotation - 0.1
+    elseif love.keyboard.isDown("right", "d") then
+        rotation = rotation + 0.1
+    end
+end
+
+--Functions- blocks of computation can have inputs and outputs, Strings- a list of characters, numbers- numbers,
+--comments- make them by -- are notes, dont compile into the system,
+--Drawables- drawn with the draw function (refer to wiki),
 function love.draw()
-    love.graphics.print("Daddy is a derpio", 400, 300)
-    love.graphics.draw(creeper, 500, 300, math.pi, 16, 16)
+    love.graphics.print("UNDER DEVOLPMENT, THANK YOU :D", 400, 300)
+    love.graphics.draw(brillJelly, 500, 300, rotation)
 end
